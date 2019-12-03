edfecf687a9eec9ead96b73d858357d8fe3a6e1d
Leporoni <leporoni@hotmail.com>
2019-12-03 14:35:26 -0300

    first commit

---
+++ src/main/java/com/leporonitech/coursesmanageapi/CoursesManageApiApplication.java
@@ -0,0 +1,13 @@
+package com.leporonitech.coursesmanageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesManageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesManageApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leporonitech/coursesmanageapi/CoursesManageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leporonitech.coursesmanageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesManageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
